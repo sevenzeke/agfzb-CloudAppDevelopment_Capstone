d6d56f59b2a39925c44a89d9928ead6edefc8d23
sevenzeke <sevenzeke@gmail.com>
2022-12-31 03:52:37 -0500

    week 3 update (Pylint fix)

--- functions/sample/python/postreview.py
+++ functions/sample/python/postreview.py
@@ -2,15 +2,13 @@ from ibmcloudant.cloudant_v1 import CloudantV1, Document
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def main(dict):
-    API_KEY = '5WUNPZBjs84te6oUX0v4HpYOA1fE_qGGbPltrwouY8hk'
-    API_URL = 'https://51d008c1-5757-4d25-a4a7-208e2742931e-bluemix.cloudantnosqldb.appdomain.cloud'
-    DB_NAME = 'reviews'
-    authenticator = IAMAuthenticator(API_KEY)
+    api_key = '5WUNPZBjs84te6oUX0v4HpYOA1fE_qGGbPltrwouY8hk'
+    api_url = 'https://51d008c1-5757-4d25-a4a7-208e2742931e-bluemix.cloudantnosqldb.appdomain.cloud'
+    db_name = 'reviews'
+    authenticator = IAMAuthenticator(api_key)
     service = CloudantV1(authenticator=authenticator)
-    service.set_service_url(API_URL)
-    
+    service.set_service_url(api_url)
     newreview = dict["review"]
-    
     newreview = Document(
         name=newreview['name'],
         dealership=int(newreview['dealership']),
@@ -21,14 +19,17 @@ def main(dict):
         car_make=newreview['car_make'],
         car_model=newreview['car_model'],
         car_year=int(newreview['car_year']))
-    
-    response = service.post_document(db=DB_NAME, document=newreview).get_result()
-    
+    response = service.post_document(
+        db=db_name,
+        document=newreview).get_result()
     try:
         code = 200
-        if response['ok'] == False:
+        if response['ok'] is False:
             code = 404
-              
-        return {'statusCode': code, 'headers': { 'Content-Type': 'application/json' }, 'body': response}
+        return {'statusCode': code,
+                'headers': { 'Content-Type': 'application/json' },
+                'body': response}
     except:
-        return {'statusCode': 500, 'headers': { 'Content-Type': 'application/json' }, 'body': {}}
+        return {'statusCode': 500,
+                'headers': { 'Content-Type': 'application/json' },
+                'body': {}}
